d6ed1fa67654bae8f7c47c799c6bcbdaada4e1de
Ellie Cohen <ecohen2@huskers.unl.edu>
2023-08-24 13:25:40 -0500

    added partner code

--- hello.c
+++ hello.c
@@ -6,5 +6,8 @@ int main(int argc, char **argv) {
   printf("Max Poore!\n");
   printf("Computer Science\n");
 
+  printf("Ellie Cohen!\n");
+  printf("Major is computer science!\n");
+
   return 0;
 }
